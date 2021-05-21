17effedb762060d5212550c3967c3ecc7d012363
Shilpi Bhunia <80779369+Shilpi96@users.noreply.github.com>
2021-05-21 11:03:58 +0100

    Update GOES_download_data.py

--- GOES_download_data.py
+++ GOES_download_data.py
@@ -14,13 +14,13 @@ import sunpy.timeseries as ts
 
 tr = TimeRange(['2014-09-28 02:00', '2014-09-28 04:00'])
 results = Fido.search(a.Time(tr), a.Instrument.xrs)
-files = Fido.fetch(results)
+files = Fido.fetch(results, path = 'path of the directory', overwrite = False)
 
 ####### Creating the timeseries
 
 goes_ts = ts.TimeSeries(files, source="XRS")
 tr = TimeRange("2014/09/28T02:25:00", "2014/09/28T04:00:00") #change this to what time range you want to plot
-goes_tr = goes_ts.truncate(tr)
+goes_tr = goes_ts.truncate(tr). #### need to truncate if goes have two timeseries of GOES-13 and GOES-15
 date_format_goes = dates.DateFormatter("%H:%M")
 
 fig = plt.figure()
@@ -39,10 +39,10 @@ gax.set_yscale("log")
 gax.autoscale(axis="x", tight=True)
 gax2 = ax1.twinx()
 gax2.set_yscale("log")
-gax.set_ylim(1e-10, 1e-3)
-gax2.set_ylim(1e-10, 1e-3)
-gax2.set_yticks((1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3))#((1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2))
-gax2.set_yticklabels((' ', ' ', 'A', 'B', 'C', 'M', 'X', ' '))#((' ', 'A', 'B', 'C', 'M', 'X', ' '))
+gax.set_ylim(1e-9, 1e-3)
+gax2.set_ylim(1e-9, 1e-3)
+gax2.set_yticks((1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3))#((1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2))
+gax2.set_yticklabels(('A', 'B', 'C', 'M', 'X', ' '))#((' ', 'A', 'B', 'C', 'M', 'X', ' '))
 handles, labels = gax.get_legend_handles_labels()
 handles.reverse()
 
