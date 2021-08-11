8370b537fd3a6ac8470392b78b23c6dc0fc9bdfb
Shilpi Bhunia <80779369+Shilpi96@users.noreply.github.com>
2021-08-11 09:35:17 +0100

    Update AIA_with_radio_points.py

--- AIA_scripts/AIA_with_radio_points.py
+++ AIA_scripts/AIA_with_radio_points.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+mport matplotlib.pyplot as plt
 import numpy as np
 import glob
 import matplotlib.gridspec as gridspec
@@ -30,64 +30,199 @@ from matplotlib.patches import Rectangle
 from matplotlib import cm
 
 
+### Function to get the positions of the radio sources
 def getarcsec(gauss_file, fint=0, tint=0):
 
-	pos = np.load(gauss_file)
+	pos = gauss_file	
 
 	xasec = pos[fint][tint][0]*u.arcsec
 	yasec = pos[fint][tint][1]*u.arcsec
 
 	return xasec.to('deg'), yasec.to('deg')
 
-########Loading the AIA file
+### function to get the ratio data
+def getratio(data, f=0,i=0, vmin=0.5, vmax=1.5):
+
+	
+        #----------------------#
+        #     Get ratio
+        #
+	ratio = data[f]/data[i]
+
+        #------------------------------------#
+        #     Remove NaNs and infinites
+        #
+	ratio = np.nan_to_num(ratio)
+	ratio[np.where(ratio>1e3)] = 1.0
+	ratio[np.where(ratio<-1e3)] = 1.0
+	ratio = np.clip(ratio, vmin, vmax)
+        
+        #------------------------------------#
+        #     Map the intensity scale to between 0 -> 1 for the RGB array
+        #     (The RGB plotter expects intensity values from 0 to 1.
+	imscale = np.linspace(vmin, vmax, len(ratio))
+	newscale = np.linspace(0.0, 1.0, len(ratio))
+	iscale_img = np.interp(ratio, imscale, newscale)
+	return iscale_img
+
+######## Creating the RGB array and get the aia 171 map
 
 root = '/mnt/LOFAR-PSP/shilpi_MWA_project/'
-aia_map = sunpy.map.Map(root+'AIA_data/aiaimages_171/aia_lev1_171a_2014_09_28t02_47_35_34z_image_lev1.fits')
+#aia_map = sunpy.map.Map(root+'AIA_data/aiaimages_171/aia_lev1_171a_2014_09_28t02_47_35_34z_image_lev1.fits')
+data_171 = np.load(root+'AIA_data/AIA_normailse_data/data_171.npy')
+data_193 = np.load(root+'AIA_data/AIA_normailse_data/data_193.npy')
+data_211 = np.load(root+'AIA_data/AIA_normailse_data/data_211.npy')
+
+f = 80
+i = 70
+ratio_a = getratio(data_171, f=f,i=i)
+ratio_b = getratio(data_193, f=f,i=i)
+ratio_c = getratio(data_211, f=f,i=i)
+
+rgb = np.zeros((4096, 4096, 3))
+rgb[:, :, 0] = ratio_a
+rgb[:, :, 1] = ratio_b
+rgb[:, :, 2] = ratio_c
+
+### load the time from the aia files
+time_171 = np.load(root+'AIA_data/AIA_normailse_data/Time_171.npy')
+aia_171_map = sunpy.map.Map(root+'AIA_data/aiaimages_171/aia_lev1_171a_2014_09_28t'+time_171[f])
+#pdb.set_trace()
 
 ############## Defining Axes
 
-fig = plt.figure(figsize=(13, 7))
+fig = plt.figure(figsize=(10, 7))
 outer = gridspec.GridSpec(2, 3, wspace=0.5, hspace=0.2)
 outer.update(left=0.1, right=0.9, top=0.95, bottom=0.05, wspace=0.3)
 inner0 = gridspec.GridSpecFromSubplotSpec(1, 1,
-        subplot_spec=outer[0:,:1], wspace=0.1, hspace=0.3)
+        subplot_spec=outer[0:2,:1], wspace=0.1, hspace=0.3)
 
-ax1 = plt.subplot(inner0[0], projection=aia_map)
+ax1 = plt.subplot(inner0[0], projection=aia_171_map)
 
-sdoaia171 = plt.get_cmap('sdoaia171')
-aia_map.plot(cmap = sdoaia171, norm=ImageNormalize(vmin=0,vmax=5000,stretch=LogStretch(10)))
-
+ax1.imshow(rgb)
+aia_171_map.draw_grid(axes=ax1,annotate=False)
 ##### Setting limit in both x and y axis
-xlims_world = [-350, 1250]*unit.arcsec
+xlims_world = [-350, 1225]*unit.arcsec
 ylims_world = [-2000, 200]*unit.arcsec
-world_coords = SkyCoord(Tx=xlims_world, Ty=ylims_world, frame=aia_map.coordinate_frame)
-pixel_coords = aia_map.world_to_pixel(world_coords)
+world_coords = SkyCoord(Tx=xlims_world, Ty=ylims_world, frame=aia_171_map.coordinate_frame)
+pixel_coords = aia_171_map.world_to_pixel(world_coords)
 # we can then pull out the x and y values of these limits.
 xlims_pixel = pixel_coords.x.value
 ylims_pixel = pixel_coords.y.value
 ax1.set_xlim(xlims_pixel)
 ax1.set_ylim(ylims_pixel)	
 ax1.patch.set_facecolor('black')
-
-from matplotlib import pylab
-import matplotlib.colors as colors
-cm = pylab.get_cmap('cool_r')
-collist = cm(np.linspace(0, 255, 10).astype(int))
-
+ax1.set_xlabel('X (arcsec)')
+ax1.set_ylabel('Y (arcsec)')
+ax1.set_title(aia_171_map.meta['date-obs'])
 
 
 ##############################################################################
-##     Overplot the MWA points.
+#     Overplot the MWA points.
+cm = pylab.get_cmap('Blues')
+collist = cm(np.linspace(0, 255, 10).astype(int))   #### Creating the colour points in a step 10 from 0 to 255 considering you have 10 points to plot
+
 gauss_file = np.load(root+'CLEAN1_1095907576/new_analysis/103-104/coords_in_arcsec.npy')
 xasec, yasec = getarcsec(gauss_file, fint=0, tint=173)
 ax1.plot(xasec, yasec, 'o', color=collist[0], transform=ax1.get_transform('world'))
 
 
-
-plt.show()
-
+####### Defining Axes for plotting two spectras
 inner1 = gridspec.GridSpecFromSubplotSpec(1, 1,
         subplot_spec=outer[0:1,1:], wspace=0.1, hspace=0.04)           ##### from 0th row and 1st column  
 
 inner2 = gridspec.GridSpecFromSubplotSpec(6, 1,
         subplot_spec=outer[1:2,1:], wspace=0.1, hspace=0.05)
+
+
+######## plotting the MWA dynamic spectra
+spectra = np.load('/mnt/LOFAR-PSP/shilpi_MWA_project/1095907576/dynamic_spectrum_from_interferometric_data/spectra.npy')
+  
+freq = np.load('/mnt/LOFAR-PSP/shilpi_MWA_project/1095907576/dynamic_spectrum_from_interferometric_data/freq.npy')
+print('Calculating times ...')
+start_time = datetime.datetime(2014, 9, 28, 2, 46, 6, 0)
+timestep = timedelta(seconds = 0.5)
+length  = spectra.shape[2]
+times = [(start_time + i*timestep) for i in range(length)]
+times_mpl = [dates.date2num(t) for t in times]
+       
+ylabels = [[132.5], [119.5], 
+            [108.0], [ 98.0], 
+            [ 89.0], [ 80.0]]
+
+vmin = np.log10(1000)
+vmax = np.log10(180000)
+t0 = dates.date2num(times[0])
+t1 = dates.date2num(times[-1])
+TIME = [(datetime.datetime(2014, 9, 28, 2, 47) +i*timedelta(seconds = 60)) for i in range(9)]
+	
+for i in range(6):
+	
+	f0 = freq[i][0][0]
+	f1 = freq[i][-1][0]
+	
+	ax = plt.Subplot(fig, inner2[i])
+	peak = ax.imshow(spectra[i],cmap=plt.get_cmap('Spectral_r'),
+        vmin=vmin, vmax = vmax, aspect = 'auto', origin = 'lower',
+        extent=(t0, t1, f0, f1))
+	ax.text(0.5, 0.5,"MWA" , fontsize = 10, color ="black")
+	
+	ax.set_yticks(ylabels[i])
+	ax.set_yticklabels(ylabels[i])
+	ax.yaxis.set_tick_params(labelsize=10)
+	ax.set_xticklabels([])
+	## Adding verticle lines on the spectra
+	for j in range(len(TIME)):
+		ax.axvline(x= TIME[j], color='black', linestyle='dotted', linewidth = 0.5)
+	
+		
+	ax.xaxis_date()
+	ax.xaxis.set_major_locator(dates.MinuteLocator())
+	## specify the time format for x-axis
+	ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
+	ax.set_xlabel('Time (UT)')
+	if i == 4 :ax.set_ylabel('Frequency (MHz)')
+	fig.add_subplot(ax)
+
+######## Plotting the Learmonth dynamic spectra
+
+spectro = np.load('/mnt/LOFAR-PSP/shilpi_MWA_project/GOES_learmonth/learmonth_data/learmonth_spectra.npy')
+times1 = np.load('/mnt/LOFAR-PSP/shilpi_MWA_project/GOES_learmonth/learmonth_data/times.npy')
+freqs = np.load('/mnt/LOFAR-PSP/shilpi_MWA_project/GOES_learmonth/learmonth_data/freq.npy')
+
+#The Following takes care of the difference in epochs for time in IDL and Python.
+idl_ints_epoch = datetime.datetime(1979, 1, 1, 0, 0, 0)  
+unix_tai_epoch = datetime.datetime(1970, 1, 1, 1, 0, 0)
+tdelt = (idl_ints_epoch - unix_tai_epoch).total_seconds()
+times1 = [t+tdelt for t in times1]
+times1 = np.array(times1)
+
+dtimes = [datetime.datetime.fromtimestamp(t) for t in times1]
+start = dates.date2num(dtimes[5492])
+end = dates.date2num(dtimes[5686])
+width = end - start
+# Choose plot times
+t0plot = datetime.datetime(2014, 9, 28, 2, 46, 6)
+t1plot = datetime.datetime(2014, 9, 28, 2, 55,48)
+
+
+ax2 = plt.subplot(inner1[0])
+ax2.pcolormesh(dtimes, freqs, spectro, cmap = plt.get_cmap('Spectral_r'),vmin = np.percentile(spectro, 80),vmax = np.percentile(spectro, 99))
+
+ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+#ax2.xaxis.tick_top()
+ax2.set_xlim(t0plot, t1plot)
+ax2.set_ylim(freqs[415], freqs[629])
+
+##### Drawing transparent rectangles to show the MWA channels on the Learmonth spectra
+freq = [131.2,118.4,106.88,97.0,87.68,78.72]
+for i in range(len(freq)):
+	rect = Rectangle((start, freq[i]), width, 2.52, facecolor='white', alpha = 0.3, edgecolor='black', linewidth = 1)
+	ax2.add_patch(rect) 
+
+##### Adding verticle dotted lines on the plot
+for j in range(len(TIME)):
+	ax.axvline(x= TIME[j], color='black', linestyle='dotted', linewidth = 0.5)
+
+plt.show()
+
