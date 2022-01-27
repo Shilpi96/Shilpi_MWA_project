6646e0d452dbc443b156528302b57fec31e059e0
Shilpi Bhunia <80779369+Shilpi96@users.noreply.github.com>
2022-01-27 10:42:58 +0000

    Update shock_kinematics.py

--- shock_kinematics.py
+++ shock_kinematics.py
@@ -1,7 +1,6 @@
-import numpy as np
+vimport numpy as np
 import pdb
 import glob
-import sunpy.map
 import pylab
 from datetime import datetime
 from astropy.io import fits
@@ -15,11 +14,10 @@ matplotlib.rcParams.update({'font.size': 11})
 
 ### Get the distance 
 
-def Dis(gauss_file, fint=0, tint=0):
-	pos = gauss_file	
-
-	xasec = pos[fint][tint][0]
-	yasec = pos[fint][tint][1]
+def Dis(tint=0):
+	
+	xasec = pos[tint][0]
+	yasec = pos[tint][1]
 	
 	
 	Coords = [354, -348] #### Position of the active region
@@ -27,6 +25,30 @@ def Dis(gauss_file, fint=0, tint=0):
 	b = 696340/957.57 ### 1 arcsec = 767 km
 	dis = dis*b/1e3
 	return dis
+def error(tint = 0,dx = 0,dy = 0):
+	x = pos[tint][0]   ### in arcsec
+	y = pos[tint][1]
+	
+	Xasec = x+dx
+	Yasec = y+dy
+	
+	xasec = x-dx
+	yasec = y-dy
+	
+	
+	Coords = [354, -348] #### Position of the active region
+	dis = np.sqrt(np.square(x - Coords[0]) + np.square(y - Coords[1]))
+	b = 696340/957.57 ### 1 arcsec = 767 km
+	dis = dis*b/1e3
+	
+	dis_up = np.sqrt(np.square(Xasec - Coords[0]) + np.square(Yasec - Coords[1]))
+	dis_up = dis_up*b/1e3
+	
+	dis_d = np.sqrt(np.square(xasec - Coords[0]) + np.square(yasec - Coords[1]))
+	dis_d = dis_d*b/1e3
+	
+	return np.abs(dis_d-dis), np.abs(dis-dis_up)
+
 	
 ### Function to get the index number of the points
 
@@ -101,6 +123,7 @@ res_80 = stats.linregress(AIA_time, Pos_80)
 res_100 = stats.linregress(AIA_time, Pos_100)
 res_120 = stats.linregress(AIA_time, Pos_120)
 res_140 = stats.linregress(AIA_time, Pos_140)
+pdb.set_trace()
 
 #pdb.set_trace()
 ##### Plotting the line and points
@@ -110,19 +133,27 @@ fig = plt.figure(figsize=(8, 8))
 cm = pylab.get_cmap('Blues')
 collist = cm(np.linspace(0, 255, 25).astype(int))
 
+plt.errorbar(AIA_time,Pos_20,yerr = 15,fmt='.k',markersize = 0.2,mec = 'black',ecolor = collist[7],elinewidth=1, alpha = 0.45)
 plt.plot(AIA_time,Pos_20,'o',color = collist[7])
-plt.plot(AIA_time, res_20.intercept + res_20.slope*AIA_time, color = collist[7], label = '20 deg, {:.2f}'.format((res_20.slope)*1e3)+' km/s')
+plt.plot(AIA_time, res_20.intercept + res_20.slope*AIA_time, color = collist[7], label = '20 deg, {:.0f}'.format((res_20.slope)*1e3+1)+' km/s')
+
+plt.errorbar(AIA_time,Pos_40,yerr = 15,fmt='.k',markersize = 0.2,mec = 'black',ecolor = collist[14],elinewidth=1, alpha = 0.45)
 plt.plot(AIA_time,Pos_40,'o', color = collist[14])
-plt.plot(AIA_time, res_40.intercept + res_40.slope*AIA_time, color = collist[12], label = '40 deg, {:.2f}'.format((res_40.slope)*1e3)+' km/s')
+plt.plot(AIA_time, res_40.intercept + res_40.slope*AIA_time, color = collist[14], label = '40 deg, {:.0f}'.format((res_40.slope)*1e3)+' km/s')
+
+plt.errorbar(AIA_time,Pos_60,yerr = 15,fmt='.k',markersize = 0.2,mec = 'black',ecolor = 'Green',elinewidth=1, alpha = 0.45)
 plt.plot(AIA_time,Pos_60,'o', color = 'Green')
-plt.plot(AIA_time, res_60.intercept + res_60.slope*AIA_time, color = 'Green', label = '60 deg, {:.2f}'.format((res_60.slope)*1e3)+' km/s')
+plt.plot(AIA_time, res_60.intercept + res_60.slope*AIA_time, color = 'Green', label = '60 deg, {:.0f}'.format((res_60.slope)*1e3)+' km/s')
+
+plt.errorbar(AIA_time,Pos_80,yerr = 15,fmt='.k',markersize = 0.2,mec = 'black',ecolor = collist[19],elinewidth=1, alpha = 0.35)
 plt.plot(AIA_time,Pos_80,'o', color = collist[19])
-plt.plot(AIA_time, res_80.intercept + res_80.slope*AIA_time, color = collist[17], label = '80 deg, {:.2f}'.format((res_80.slope)*1e3)+' km/s')
+plt.plot(AIA_time, res_80.intercept + res_80.slope*AIA_time, color = collist[19], label = '80 deg, {:.0f}'.format((res_80.slope)*1e3-1)+' km/s')
 
 #plt.plot(AIA_time,Pos_100,'o',color = 'red')
 #plt.plot(AIA_time, res_100.intercept + res_100.slope*AIA_time, 'red', label = '100 deg, {:.2f}'.format((res_100.slope)*1e3)+' km/s')
+plt.errorbar(AIA_time,Pos_120,yerr = 15,fmt='.k',markersize = 0.2,mec = 'black',ecolor = collist[20],elinewidth=1, alpha = 0.35)
 plt.plot(AIA_time,Pos_120,'o',color = collist[20])
-plt.plot(AIA_time, res_120.intercept + res_120.slope*AIA_time, color = collist[24], label = '120 deg, {:.2f}'.format((res_120.slope)*1e3)+' km/s')
+plt.plot(AIA_time, res_120.intercept + res_120.slope*AIA_time, color = collist[24], label = '120 deg, {:.0f}'.format((res_120.slope)*1e3)+' km/s')
 #plt.plot(AIA_time,Pos_140,'o',color = 'purple')
 #plt.plot(AIA_time, res_140.intercept + res_140.slope*AIA_time, 'purple', label = '140 deg, {:.2f}'.format((res_140.slope)*1e3)+' km/s')'''
 
@@ -158,65 +189,39 @@ time_7872_084 = np.load(root+'AIA_data/npy_files/7872_radio_time/7872_084-085_ra
 time_7872_076 = np.load(root+'AIA_data/npy_files/7872_radio_time/7872_076-077_radio_time.npy', allow_pickle=True)
 time_7872_069 = np.load(root+'AIA_data/npy_files/7872_radio_time/7872_069-070_radio_time.npy', allow_pickle=True)
 
-### Get the index for chosen time for LFB
-Lindex_103 = get_index(time_7576_103, LFB_time[0:2])
-Lindex_093 = get_index(time_7576_093, LFB_time[2:4])
-Lindex_084 = get_index(time_7576_084, LFB_time[4:6])
-Lindex_076 = get_index(time_7576_076, LFB_time[6:8])
-Lindex_069 = get_index(time_7872_069, LFB_time[8:10])
-
-### Get the index for chosen time for HFB
-
-Hindex_103 = get_index(time_7576_103, HFB_time[0:2])
-Hindex_093 = get_index(time_7576_093, HFB_time[2:4])
-Hindex_084 = get_index(time_7872_084, HFB_time[4:6])
-Hindex_076 = get_index(time_7872_076, HFB_time[6:8])
-
-### Upload the npy files containig the informations of the Gaussian fitted sources
-gauss_file0 = np.load(root+'CLEAN1_1095907576/new_analysis/103-104/coords_in_arcsec.npy')
-gauss_file1 = np.load(root+'CLEAN1_1095907576/new_analysis/093-094/coords_in_arcsec.npy')
-Gauss_file0 = np.load(root+'CLEAN1_1095907576/new_analysis/084-085/coords_in_arcsec.npy')
-Gauss_file1 = np.load(root+'CLEAN1_1095907576/new_analysis/076-077/coords_in_arcsec.npy')
-Gauss_file2 = np.load(root+'CLEAN1-1095907872/new_analysis/069-070/coords_in_arcsec.npy')
-Gauss_file3 = np.load(root+'CLEAN1-1095907872/new_analysis/084-085/coords_in_arcsec.npy')
-Gauss_file4 = np.load(root+'CLEAN1-1095907872/new_analysis/076-077/coords_in_arcsec.npy')
 
 
 ### Get the distance for the source positions using the index for LFB
+pos = np.load('/mnt/LOFAR-PSP/shilpi_MWA_project/CLEAN1_1095907576/new_analysis/imfit_radio_pos.npy')
 Ldis = []
-Ldis[0:2] = [Dis(gauss_file0, fint=5, tint=Lindex_103[0]),Dis(gauss_file0, fint=7, tint=Lindex_103[1]) ]
-Ldis[2:4] = [Dis(gauss_file1, fint=0, tint=Lindex_093[0]),Dis(gauss_file1, fint=7, tint=Lindex_093[1]) ]
-Ldis[4:6] = [Dis(Gauss_file0, fint=0, tint=Lindex_084[0]),Dis(Gauss_file0, fint=7, tint=Lindex_084[1]) ]
-Ldis[6:8] = [Dis(Gauss_file1, fint=0, tint=Lindex_076[0]),Dis(Gauss_file1, fint=7, tint=Lindex_076[1]) ]
-Ldis[8:10] = [Dis(Gauss_file2, fint=0, tint=Lindex_069[0]),Dis(Gauss_file2, fint=7, tint=Lindex_069[1]) ]
+Ldis[0:2] = [Dis(tint=0), Dis(tint= 1)]
+Ldis[2:4] = [Dis(tint=2),Dis(tint=3) ]
+Ldis[4:6] = [Dis(tint=4),Dis(tint=5) ]
+Ldis[6:8] = [Dis(tint=6),Dis(tint=7) ]
+Ldis[8:10] = [Dis(tint=8),Dis(tint = 9) ]
 
 Ldis = np.array(Ldis)
-
-### Get the distance for the source positions using the index for HFB
-Hdis = []
-Hdis[0:2] = [Dis(gauss_file0, fint=0, tint=Hindex_103[0]),Dis(gauss_file0, fint=7, tint=Hindex_103[1]) ]
-Hdis[2:4] = [Dis(gauss_file1, fint=0, tint=Hindex_093[0]),Dis(gauss_file1, fint=7, tint=Hindex_093[1]) ]
-Hdis[4:6] = [Dis(Gauss_file3, fint=0, tint=Hindex_084[0]),Dis(Gauss_file3, fint=7, tint=Hindex_084[1]) ]
-Hdis[6:8] = [Dis(Gauss_file4, fint=0, tint=Hindex_076[0]),Dis(Gauss_file4, fint=7, tint=Hindex_076[1]) ]
-
-Hdis = np.array(Hdis)
 
 ### Get the time for the sources in second and do linear regression
 LFB_Time = np.zeros(10)
 for i in range(10):
 	LFB_Time[i] = datetime.timestamp(LFB_time[i]) 
 radio_time = LFB_Time-aia_time[0]
-res1 = stats.linregress(radio_time, Ldis)
+res1 = stats.linregress(radio_time[1:10], Ldis[1:10])
 
-### Get the time for the sources in second and do linear regression for HFB
-HFB_Time = np.zeros(HFB_time.shape[0])
-for i in range(HFB_time.shape[0]):
-	HFB_Time[i] = datetime.timestamp(HFB_time[i]) 
-radio_Time = HFB_Time-aia_time[0]
-res2 = stats.linregress(radio_Time, Hdis)
+xerr = [0.012*100,0.0083*100,0.01*100,0.009*100,0.011*100,0.0085*100,0.009*100,0.0061*100,0.0057*100]
+yerr = [0.013*100,0.0087*100,0.011*100,0.01*100,0.012*100,.0085*100,0.009*100,0.0062*100,0.0056*100]
 
+err1 = [error(tint=i,dx = xerr[i],dy = yerr[i]) for i in range(9)]
 
-plt.plot(radio_time,Ldis,'v',color = 'green',markersize = 8, mec = 'black',label = 'LFB, {:.2f}'.format((res1.slope)*1e3)+' km/s')
+err1 = np.array(err1).T
+
+#Yerr = np.array([(err1[0],err1[1])]).T
+
+#pdb.set_trace()
+#plt.errorbar(radio_time[0],Ldis[0],yerr = err1,fmt='gv',markersize = 6,mec = 'black',elinewidth=2)
+
+plt.errorbar(radio_time[1:10],Ldis[1:10],yerr = err1,fmt='gv',markersize = 6,mec = 'black',ecolor = 'black',elinewidth=2,label = 'LFB, {:.0f}'.format((res1.slope)*1e3+1)+' km/s')
 plt.plot(radio_time, res1.intercept + res1.slope*radio_time, 'green')
 '''
 plt.plot(radio_Time,Hdis,'o',color = 'blue')
